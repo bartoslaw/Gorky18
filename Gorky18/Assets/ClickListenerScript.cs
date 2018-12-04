5fe1aa65a6487acbbec795fea55bb5e7a73745ff
Bartłomiej Smektała <bartlomiejsmektala@MacBook-Pro-Bartomiej-2.local>
2018-12-04 22:07:15 +0100

    Moving player by clicking mouse, trivial and still buggy floor marking

---
+++ Gorky18/Assets/ClickListenerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IsoTools;
+using IsoTools.Physics;
+
+public class ClickListenerScript : MonoBehaviour {
+
+	IsoWorld world;
+	
+	// Use this for initialization
+	void Start () {
+		world = IsoWorld.GetWorld(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 mousePosition = world.MouseIsoTilePosition();
+			
+			PlayerControllerScript playerController = FindObjectOfType<PlayerControllerScript>();
+			Vector3 playerPosition = playerController.GetComponent<IsoObject>().position;
+
+			Vector3 newPosition = new Vector3(mousePosition.x, mousePosition.y - 0.5f, playerPosition.z);
+
+			playerController.GetComponent<IsoObject>().position = newPosition;
+			playerController.AdjustMovableTiles();
+		}
+	}
+}
